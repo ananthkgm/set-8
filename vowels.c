add0b9554e578f713d9756cc4f19ca6d1d88b28a
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-02-20 10:59:31 +0000

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	char a[50];
+	int i,flag=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	 if(a[i]=='a' || a[i]=='e' ||a[i]=='i' ||a[i]=='o' || a[i]=='u')
+	  flag++;
+	  if(flag>0)
+	  {
+	  printf("yes");
+	  }
+	  else
+	  {
+	  printf("no");
+	  }
+	return 0;
+}
