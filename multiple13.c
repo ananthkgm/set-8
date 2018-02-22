2a88ef0b7e1fba874dac06742976e1ceb31d7591
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-02-22 10:42:10 +0000

    Create multiple13.c

---
+++ multiple13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n%13==0)
+	{
+	printf("%d is multiple of 13",n);
+	}
+	else
+	{
+	printf("%d is not multiple of 13",n);
+	}
+	return 0;
+}
